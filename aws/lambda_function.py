98b10153c82a20e91122790dc94587b1a988ebe5
MysticGems <MysticGems@users.noreply.github.com>
2022-04-27 11:32:19 -0400

    new: query DynamoDB for reads

--- aws/lambda_function.py
+++ aws/lambda_function.py
@@ -1,13 +1,32 @@
 import json
 import boto3
 
+TABLE = 'http-test'
+
+def get_value(key):
+    dynamo = boto3.client('dynamodb')
+    try:
+        response = dynamo.get_item(
+            TableName=TABLE,
+            Key={
+                'key': {
+                    'S': key
+                    }
+                }
+            )
+    except:
+        return '1,13' # XP_STORAGE_EXCEPTION
+    if 'Item' in response:
+        return '0,' + response['Item']['value']['S'] # XP_ERROR_NONE
+    return '1,14' # XP_ERROR_KEY_NOT_FOUND
+
 def lambda_handler(event, context):
-    # TODO implement
     if event['requestContext']['http']['method'] == 'GET':
         data_key = event['requestContext']['http']['path'].lstrip('/')
+        key_value = get_value( data_key )
         return {
             'statusCode': 200,
-            'body': data_key
+            'body': key_value
         }
     return {
         'statusCode': 403,

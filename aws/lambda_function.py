6167319dda4fe6bb10fb1d7b2d0711ec7a1a8e59
Mystic Gemss <mystic.gems.sl@gmail.com>
2022-05-03 00:56:24 -0400

    - new: authentication - chore: put table reference where it belongs - chore: docstrings

--- aws/lambda_function.py
+++ aws/lambda_function.py
@@ -1,14 +1,18 @@
+"""
+Simple queries and updates to SLDB-type key-value database
+"""
 import os
-import json
+import hashlib
 import boto3
 
-TABLE = 'narf'
-
 def get_value(key):
+    """
+    Actually fetch data from the database
+    """
     dynamo = boto3.client('dynamodb')
     try:
         response = dynamo.get_item(
-            TableName=TABLE,
+            TableName=os.environ['table'],
             Key={
                 'key': {
                     'S': key
@@ -21,17 +25,59 @@ def get_value(key):
         return '0,' + response['Item']['value']['S'] # XP_ERROR_NONE
     return '1,14' # XP_ERROR_KEY_NOT_FOUND
 
+def get_secret(secret):
+    """
+    Get secret for authentication
+    """
+    secret_manager = boto3.client('secretsmanager')
+    try:
+        response = secret_manager.get_secret_value( SecretId=secret )
+        return response['SecretString']
+    except:
+        return ''
+
+def authenticate(data_key, headers, secret_value):
+    """
+    Authenticate the request
+    """
+    try:
+        auth_hash = hashlib.sha1(
+            headers['x-secondlife-object-key']
+            + data_key
+            + secret_value
+        )
+        authentication = auth_hash.hexdigest()
+        return authentication == headers['secure']
+    except:
+        return False
+
 def lambda_handler(event, context):
-    global TABLE
-    TABLE = os.environ['table']
+    """
+    Request handler
+    """
     if event['requestContext']['http']['method'] == 'GET':
         data_key = event['requestContext']['http']['path'].lstrip('/')
+        secret = get_secret(os.environ['secret'])
+        if not secret:
+            return {
+                'statusCode': 200,
+                'headers': {'Content-Type': 'text/plain' },
+                'body': '1,13' # XP_STORAGE_EXCEPTION
+            }
+        if not authenticate( key_value, event['headers'], secret ):
+            return {
+                'statusCode': 200,
+                'headers': {'Content-Type': 'text/plain' },
+                'body': '1,4' # XP_ERROR_NOT_PERMITTED
+            }
         key_value = get_value( data_key )
         return {
             'statusCode': 200,
+            'headers': {'Content-Type': 'text/plain' },
             'body': key_value
         }
     return {
         'statusCode': 403,
+        'headers': {'Content-Type': 'text/plain' },
         'body': '403 forbidden'
     }

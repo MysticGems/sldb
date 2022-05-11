78135d1f760047674524054773e6ac975a2330f3
MysticGems <MysticGems@users.noreply.github.com>
2022-05-11 00:32:00 -0400

    chore: logging

--- aws/lambda_function.py
+++ aws/lambda_function.py
@@ -6,6 +6,9 @@ import logging
 import hashlib
 import boto3
 
+# Configure logging
+logging.getLogger().setLevel(logging.INFO)
+
 def get_value(key):
     """
     Actually fetch data from the database
@@ -20,10 +23,12 @@ def get_value(key):
                     }
                 }
             )
-    except:
+    except ClientError as e:
+        logging.warning( 'Error talking to DynamoDB: %s', e )
         return '0,13' # XP_STORAGE_EXCEPTION
     if 'Item' in response:
         return '1,' + response['Item']['value']['S'] # XP_ERROR_NONE
+    logging.info( 'Key not found' )
     return '0,14' # XP_ERROR_KEY_NOT_FOUND
 
 def put_value(key, value):
@@ -45,7 +50,8 @@ def put_value(key, value):
                     }
                 }
             )
-    except:
+    except ClientError as e:
+        logging.warning( 'Error talking to DynamoDB: %s', e )
         return '0,13' # XP_STORAGE_EXCEPTION
     return '1,' + value # XP_ERROR_NONE
 
@@ -60,8 +66,10 @@ def authenticate(data_key, headers, secret_value):
             + secret_value
         )
         authentication = auth_hash.hexdigest()
+        logging.info( 'Auth hash %s, header %s' )
         return authentication == headers['secure']
     except:
+        logging.info('Error attempting authorization')
         return False
         
 BAD_REQUEST = {
@@ -87,6 +95,7 @@ def lambda_handler(event, context):
         return BAD_REQUEST
     data_key = event['requestContext']['http']['path'].lstrip('/')
     if not authenticate( data_key, event['headers'], os.environ['secret'] ):
+        logging.warning('Not authorized')
         return {
             'statusCode': 200,
             'headers': {'Content-Type': 'text/plain' },
@@ -94,6 +103,7 @@ def lambda_handler(event, context):
         }
     if event['requestContext']['http']['method'] == 'GET':
         key_value = get_value( data_key )
+        logging.info('Retrieved %s', data_key)
         return {
             'statusCode': 200,
             'headers': {'Content-Type': 'text/plain' },
@@ -101,17 +111,20 @@ def lambda_handler(event, context):
         }
     if event['requestContext']['http']['method'] == 'PUT':
         if 'body' not in event:
+            logging.warn('Update without payload')
             return {
                 'statusCode': 400,
                 'headers': {'Content-Type': 'text/plain' },
                 'body': '400 Bad Request: No payload'
                 }
         key_value = put_value( data_key, event['body'] )
+        logging.info('Updated %s', data_key)
         return {
             'statusCode': 200,
             'headers': {'Content-Type': 'text/plain' },
             'body': key_value
         }
+    logging.warning('Unrecognized request method')
     return {
         'statusCode': 501,
         'headers': {'Content-Type': 'text/plain' },

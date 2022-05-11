695c9ffd82ba946ad414b612fa281689457015da
MysticGems <MysticGems@users.noreply.github.com>
2022-05-11 02:45:48 -0400

    fix: authentication encoding problems

--- aws/lambda_function.py
+++ aws/lambda_function.py
@@ -2,12 +2,10 @@
 Simple queries and updates to SLDB-type key-value database
 """
 import os
+import json
 import logging
 import hashlib
 import boto3
-
-# Configure logging
-logging.getLogger().setLevel(logging.INFO)
 
 def get_value(key):
     """
@@ -23,12 +21,10 @@ def get_value(key):
                     }
                 }
             )
-    except ClientError as e:
-        logging.warning( 'Error talking to DynamoDB: %s', e )
+    except:
         return '0,13' # XP_STORAGE_EXCEPTION
     if 'Item' in response:
         return '1,' + response['Item']['value']['S'] # XP_ERROR_NONE
-    logging.info( 'Key not found' )
     return '0,14' # XP_ERROR_KEY_NOT_FOUND
 
 def put_value(key, value):
@@ -50,8 +46,7 @@ def put_value(key, value):
                     }
                 }
             )
-    except ClientError as e:
-        logging.warning( 'Error talking to DynamoDB: %s', e )
+    except:
         return '0,13' # XP_STORAGE_EXCEPTION
     return '1,' + value # XP_ERROR_NONE
 
@@ -60,16 +55,14 @@ def authenticate(data_key, headers, secret_value):
     Authenticate the request
     """
     try:
+        logging.debug('Authenticating %s %s', headers['x-secondlife-object-key'], data_key)
+        auth_string = headers['x-secondlife-object-key'] + data_key + secret_value
         auth_hash = hashlib.sha1(
-            headers['x-secondlife-object-key']
-            + data_key
-            + secret_value
+            auth_string.encode(encoding = 'UTF-8', errors = 'strict')
         )
         authentication = auth_hash.hexdigest()
-        logging.info( 'Auth hash %s, header %s' )
         return authentication == headers['secure']
     except:
-        logging.info('Error attempting authorization')
         return False
         
 BAD_REQUEST = {
@@ -82,6 +75,7 @@ def lambda_handler(event, context):
     """
     Request handler
     """
+    logging.debug('Event: %s', json.dumps(event))
     if 'requestContext' not in event:
         logging.error('Not an http(s) request')
         return BAD_REQUEST
@@ -95,7 +89,6 @@ def lambda_handler(event, context):
         return BAD_REQUEST
     data_key = event['requestContext']['http']['path'].lstrip('/')
     if not authenticate( data_key, event['headers'], os.environ['secret'] ):
-        logging.warning('Not authorized')
         return {
             'statusCode': 200,
             'headers': {'Content-Type': 'text/plain' },
@@ -103,7 +96,6 @@ def lambda_handler(event, context):
         }
     if event['requestContext']['http']['method'] == 'GET':
         key_value = get_value( data_key )
-        logging.info('Retrieved %s', data_key)
         return {
             'statusCode': 200,
             'headers': {'Content-Type': 'text/plain' },
@@ -111,20 +103,17 @@ def lambda_handler(event, context):
         }
     if event['requestContext']['http']['method'] == 'PUT':
         if 'body' not in event:
-            logging.warn('Update without payload')
             return {
                 'statusCode': 400,
                 'headers': {'Content-Type': 'text/plain' },
                 'body': '400 Bad Request: No payload'
                 }
         key_value = put_value( data_key, event['body'] )
-        logging.info('Updated %s', data_key)
         return {
             'statusCode': 200,
             'headers': {'Content-Type': 'text/plain' },
             'body': key_value
         }
-    logging.warning('Unrecognized request method')
     return {
         'statusCode': 501,
         'headers': {'Content-Type': 'text/plain' },
